2ff708e74b3b71a9bc5528ac8c080ecb0869545e
LidiyaZzz <Lida.ngs@mail.ru>
2023-01-22 11:09:59 +0700

    HomeWork_3

---
+++ HomeWork_3/Task-19/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 19
+/*
+  Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    14212 -> нет
+    12821 -> да
+    23432 -> да
+*/
+
+int GetValue(string message)
+{
+  int resultNumber = 0;
+  while (true)
+  {
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out resultNumber))
+    {
+      resultNumber = Math.Abs(resultNumber);
+      while (resultNumber < 9999 || resultNumber > 100000)
+      {
+        Console.WriteLine("Вы ввели не пятизначное число");
+        resultNumber = GetValue("Введите целое пятизначное число:");
+      }
+      break;
+    }
+    else
+    {
+      Console.WriteLine("Вы ввели не корректные данные. Повторите ввод!");
+    }
+  }
+  return resultNumber;
+}
+
+int number = GetValue("Введите целое пятизначное число:");
+
+void GetResult(int num)
+{
+  if (
+    (number / 10000 == number % 10) &&
+    ((number % 10000) / 1000 == (number % 100) / 10)
+  )
+  {
+    Console.WriteLine($"Вы ввели {number} - это число-паллиндром!");
+  }
+  else
+  {
+    Console.WriteLine($"Вы ввели {number} - это число не является паллиндромом.");
+  }
+}
+
+GetResult(number);
+
+
+
+
---
+++ HomeWork_3/Task-21/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// Задача 21
+/*
+  Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+    A (3,6,8); B (2,1,-7), -> 15.84
+    A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int GetNumber(string message)
+{
+  int resultNumber = 0;
+  while (true)
+  {
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out resultNumber))
+    {
+      break;
+    }
+    else
+    {
+      Console.WriteLine("Вы ввели не корректные данные. Повторите ввод!");
+    }
+  }
+  return resultNumber;
+}
+
+double GetCoordinates(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+  double result = Math.Round(Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2)), 2);
+  return result;
+}
+
+int xA = GetNumber("Введите координату X точки A:");
+int yA = GetNumber("Введите координату Y точки A:");
+int zA = GetNumber("Введите координату Z точки A:");
+
+int xB = GetNumber("Введите координату X точки B:");
+int yB = GetNumber("Введите координату Y точки B:");
+int zB = GetNumber("Введите координату Z точки B:");
+
+Console.WriteLine(GetCoordinates(xA, yA, zA, xB, yB, zB));
+
