fa0d4de142d3d60177a340448fafc406fab460a4
LidiyaZzz <Lida.ngs@mail.ru>
2023-02-02 00:20:10 +0700

    HomeWork_6

---
+++ HomeWork_6/Task-41/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 41
+/*
+  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+  вводим через один Enter (разбиваем через Split())
+    0, 7, 8, -2, -2 -> 2
+    1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.WriteLine("Введите числа через запятую:");
+string stringIn = Console.ReadLine();
+string[] strOfNum = GetStringOfNumbers(stringIn);
+int[] intNum = GetNumberArray(strOfNum);
+// PrintStringArray(strOfNum);
+Console.Write("Вы ввели числа ");
+PrintNumberArray(intNum);
+Console.WriteLine();
+Console.WriteLine($"Из введеных чисел {GetQuantityN(intNum)} являются положительными");
+
+
+// получает строковый массив чисел
+string[] GetStringOfNumbers(string inputText)
+{
+  string outputText = "";
+
+  for (int i = 0; i < inputText.Length; i++)
+  {
+    if (Char.IsLetter(inputText, i)) outputText += ',';
+    else outputText += inputText[i];
+  }
+  char[] separators = new char[] { ' ', '.', ',' };
+
+  return outputText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+}
+
+// получает массив чисел int из полностью обработанной строки
+int[] GetNumberArray(string[] strArray)
+{
+  int[] numArray = new int[strArray.Length];
+  int resultNumber = 0;
+
+  for (int i = 0; i < strArray.Length; i++)
+  {
+    if (int.TryParse(strArray[i], out resultNumber)) numArray[i] = resultNumber;
+    else continue;
+  }
+
+  return numArray;
+}
+
+// получает количество положительных чисел
+int GetQuantityN(int[] numArray)
+{
+  int count = 0;
+  foreach (int num in numArray)
+  {
+    if (num > 0) count++;
+    else continue;
+  }
+  return count;
+}
+
+void PrintStringArray(string[] array)
+{
+  Console.Write($"[");
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (i == array.Length - 1) Console.Write($"{array[i]}");
+    else Console.Write($"{array[i]}, ");
+  }
+  Console.Write($"]");
+}
+
+void PrintNumberArray(int[] array)
+{
+  Console.Write($"[");
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (i == array.Length - 1) Console.Write($"{array[i]}");
+    else Console.Write($"{array[i]}, ");
+  }
+  Console.Write($"]");
+}
+
+
---
+++ HomeWork_6/Task-43/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 43
+/*
+  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+    b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double[] coordinatesPoint = GetCoordinatesPoint(
+  GetNumber("Введите угловой коэффициент первой прямой (k1):"),
+  GetNumber("Введите смещение первой прямой по оси Y (b1):"),
+  GetNumber("Введите угловой коэффициент первой второй (k2):"),
+  GetNumber("Введите смещение первой второй по оси Y (b2):"));
+
+Console.Write("Координата точки пересечения двух прямых: ");
+PrintNumberArray(coordinatesPoint);
+
+
+double GetNumber(string message)
+{
+  double resultNumber = 0;
+  while (true)
+  {
+    Console.WriteLine(message);
+
+    if (double.TryParse(Console.ReadLine(), out resultNumber))
+    {
+      break;
+    }
+    else
+    {
+      Console.WriteLine("Вы ввели не корректные данные. Повторите ввод!");
+    }
+  }
+  return resultNumber;
+}
+
+double[] GetCoordinatesPoint(double k1, double b1, double k2, double b2)
+{
+  double xPoint = (b2 - b1) / (k1 - k2);
+  double yPoint = (k2 * b1 - k1 * b2) / (k2 - k1);
+  double[] coordinatesPoint = { xPoint, yPoint };
+
+  return coordinatesPoint;
+}
+
+void PrintNumberArray(double[] array)
+{
+  Console.Write($"(");
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (i == array.Length - 1) Console.Write($"{array[i]}");
+    else Console.Write($"{array[i]}; ");
+  }
+  Console.Write($")");
+}
+
